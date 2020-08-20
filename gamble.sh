831a7793d394df1238ccfc3efddd641f5d9c5882
Rajeev <rajeevsuchiha@gmail.com>
2020-08-20 09:16:43 +0530

    Added: Code to print resign on win/loss condition added

--- gamble.sh
+++ gamble.sh
@@ -2,11 +2,18 @@
 
 stake=100
 bet=1
-result=$(( RANDOM%2 ))
+max=$(( $stake + $(( ($stake*50)/100)) ));
+min=$((  $stake - $(( ($stake*50)/100)) ));
 
-if [ $result -eq 1 ]
-then
-	stake=$((stake+bet))
-else
-	stake=$((stake-bet))
-fi 
+while [ $stake -gt $min ] && [ $stake -lt $max ]
+do
+	result=$(( RANDOM%2 ))
+	if [ $result -eq 1 ]
+	then
+		stake=$((stake + bet))
+	else
+		stake=$((stake - bet))
+	fi
+done
+
+echo "That's all for today. Resigning..."
